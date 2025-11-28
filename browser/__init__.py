de35b701973bb8f581a2861426d5ed5508763e60
howtocuddle <sahoo.priyansu@gmail.com>
2025-11-28 10:26:17 +0000

    Delete browser directory

--- browser/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Browser Tool Package
-"""
-from .core import open_browser, close_browser, get_driver, force_cleanup
-from .dispatcher import browser_automation
-from .autonomous import autonomous_browser
-from .research import research
-from .interface import browser
-from .helpers import handle_manual_captcha
